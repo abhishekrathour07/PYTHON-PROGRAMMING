8217a5a21adeb2a971c08034ddd042a3ce0f8805
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2023-09-22 02:22:01 +0530

    Python begin

---
+++ String.py
@@ -0,0 +1,5 @@
+fruits = ["apple", "banana", "orange"]
+print(fruits[1])             # Accessing elements
+fruits.append("grape")       # Adding an element
+fruits.remove("banana")      # Removing an element
+print(fruits)
