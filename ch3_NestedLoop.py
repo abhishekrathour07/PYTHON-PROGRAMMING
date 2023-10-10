1678831c35e351ac079ecc9260c2c9af0d2eb624
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2023-10-10 16:27:29 +0530

    Nesting of loops

---
+++ ch3_NestedLoop.py
@@ -0,0 +1,11 @@
+while (i<=3):
+    for k in range(1, 4):
+        print(i, "*", k, "=", (i*k))
+    i = i + 1
+    print()
+for i in range(1,4):
+      k=1
+      while(k<=3):
+             print(i, "*", k, "=", (i*k))
+             k = k+1
+      print()
