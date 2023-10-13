f6b1fde8ad554c4b492780c7ce6f157818ec1f6a
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2023-10-14 03:45:59 +0530

    Python os

---
+++ ch6_Os_Module.py
@@ -0,0 +1,12 @@
+import os 
+import sys
+# folder = os.listdir("pythonji")
+# for folders in folder:
+#  print(folders)
+#  print(os.remove(folder))
+print(os.getcwd())
+# os.rmdir("fileos 1")
+path = "D:\MyCodes\PYTHON PROGRAMMING"
+os.chdir(path)
+print(os.getcwd())
+
