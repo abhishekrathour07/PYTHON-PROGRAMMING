5cdb782f803db99331c73014e0b860f59537fe75
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2024-01-04 04:29:23 +0530

    Python Programming

--- ch8_OVerriding .py
+++ ch8_OVerriding .py
@@ -12,13 +12,16 @@ class b:
     def about(self):
         print(f"the address is {self.address}")
 
-class c(a,b):
-    def __init__(self, name ,address, age):
-        super().__init__(name)
-        super.__init__(address)
-        self.age= age
+class c( b,a):
+    def __init__(self, name, address, age):
+        a.__init__(self, name)  # Call the __init__ of class 'a' explicitly
+        b.__init__(self, address)  # Call the __init__ of class 'b' explicitly
+        self.age = age
+
     def display(self):
-     print(f"The name is {self.name} and age is {self.age} and loaction is {self.address}")
+        print(f"The name is {self.name}, age is {self.age}, and location is {self.address}")
+
+
 
 
 obj = c('abhi','bangalore',21)
